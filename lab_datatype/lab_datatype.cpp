717d8d4f93f10102574ec85ab6433c691264071d
lebrancconvas <lebrancconvas@gmail.com>
2022-10-31 19:49:49 +0700

    💡 chore(comment): add comment to explain the output

--- lab_datatype/lab_datatype.cpp
+++ lab_datatype/lab_datatype.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	std::cout << "Size of Integer: " << sizeof(int) << std::endl;
-	std::cout << "Size of Character: " << sizeof(char) << std::endl;
-	std::cout << "Size of Float: " << sizeof(float) << std::endl;
-	std::cout << "Size of Long: " << sizeof(long) << std::endl;
-	std::cout << "Size of Boolean: " << sizeof(bool) << std::endl;
-	std::cout << "Size of String: " << sizeof(std::string) << std::endl;
+	std::cout << "Size of Integer: " << sizeof(int) << std::endl; // Size of int: 4 bytes (32 bits). 
+	std::cout << "Size of Character: " << sizeof(char) << std::endl; // Size of char: 1 byte (8 bits).
+	std::cout << "Size of Float: " << sizeof(float) << std::endl; // Size of float: 4 bytes (32 bits). 
+	std::cout << "Size of Long: " << sizeof(long) << std::endl; // Size of long: 8 bytes (64 bits). 
+	std::cout << "Size of Boolean: " << sizeof(bool) << std::endl; // Size of bool: 1 byte (8 bits). 
+	std::cout << "Size of String: " << sizeof(std::string) << std::endl; // Size of standard string: 24 bytes (192 bits). 
 	return 0;
 }
