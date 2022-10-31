fa7de5d2766d205fa99381d91894f10fa68d9f5c
lebrancconvas <lebrancconvas@gmail.com>
2022-11-01 00:41:00 +0700

    💡 chore(comment): add comment to explain code

--- lab_passby/lab_passby.cpp
+++ lab_passby/lab_passby.cpp
@@ -16,15 +16,13 @@ int increase_by_reference(int *input_reference, int increase_rate)
 int main()
 {
 	int num1 = 20;
-	int num2 = 70;
 
-	std::cout << increase_by_value(num1, 1) << std::endl;
-	std::cout << increase_by_reference(&num1, 1) << std::endl;
+	std::cout << increase_by_value(num1, 1) << std::endl; // Output: 21
+	std::cout << increase_by_reference(&num1, 1) << std::endl; // Output: 21
 
 	std::cout << "--------" << std::endl;
 
-	std::cout << num1 << std::endl;
-	std::cout << num2 << std::endl;
+	std::cout << num1 << std::endl; // num1 changed to be the input of increase_by_reference function. // Output: 21 
 
 	return 0;
 }
