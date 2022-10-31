87a03cf0abce2b49dd4703d72c999c0fa66ba745
lebrancconvas <lebrancconvas@gmail.com>
2022-10-31 19:42:23 +0700

    ✨ feat: add data types lab

---
+++ lab_datatype/lab_datatype.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Size of Integer: " << sizeof(int) << std::endl;
+	std::cout << "Size of Character: " << sizeof(char) << std::endl;
+	std::cout << "Size of Float: " << sizeof(float) << std::endl;
+	std::cout << "Size of Long: " << sizeof(long) << std::endl;
+	std::cout << "Size of Boolean: " << sizeof(bool) << std::endl;
+	std::cout << "Size of String: " << sizeof(std::string) << std::endl;
+	return 0;
+}
